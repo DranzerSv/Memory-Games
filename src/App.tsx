4f55359da8099057057f7004c50a161b0c57233d
Douglas Navarrete <dranzersv@gmail.com>
2023-03-22 17:14:28 -0600

    feature: added necesary imports to use timeMachine

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { cards } from './colors';
+
+import usePreviousState from './customHooks/usePreviousState';
+import useTimeMachine from './customHooks/useTimeMachine';
+import Card from './components/Card/Card';
+
 import './App.scss';
 
 function App() {
-  return <div className="App"></div>;
+  const [selectedCard, setSelectedCard] = useState(0);
+  // const previousCounter = usePreviousState(selectedCard);
+  const [previousValue, getPreviousValue] = useTimeMachine(selectedCard);
+
+  return (
+    <div className="app">
+      <h4>{previousValue}</h4>
+      <h3>posicion actual{selectedCard}</h3>
+      <button
+        onClick={() => {
+          setSelectedCard(selectedCard + 1);
+        }}
+      >
+        increase
+      </button>
+      <button
+        onClick={() => {
+          getPreviousValue();
+        }}
+      >
+        travelBAck
+      </button>
+      {cards.map((card) => (
+        <Card key={card['id']} id={card['id']} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
