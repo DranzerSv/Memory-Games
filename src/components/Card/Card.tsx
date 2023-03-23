2f57b7828e821aef8395aa5714ef8a041301626f
Douglas Navarrete <dranzersv@gmail.com>
2023-03-23 07:39:24 -0600

    feature: now every card has a position

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,9 +4,14 @@ import { ICardProps } from '../../interfaces/ICard';
 
 import '../Card/card.scss';
 
-export default function Card({ id }: ICardProps) {
+export default function Card({ id, setSelectedCard }: ICardProps) {
   return (
-    <div className={'square color' + id}>
+    <div
+      className={'square color' + id}
+      onClick={() => {
+        setSelectedCard(id);
+      }}
+    >
       <h1>{id}</h1>
     </div>
   );
