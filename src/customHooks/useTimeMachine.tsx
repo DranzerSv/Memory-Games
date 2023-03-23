cee684792d537704febb8c33f177d137a3a561eb
Douglas Navarrete <dranzersv@gmail.com>
2023-03-23 14:00:52 -0600

    refactor: begin new structure of useTimeMachine

--- src/customHooks/useTimeMachine.tsx
+++ src/customHooks/useTimeMachine.tsx
@@ -1,32 +1,41 @@
 import { useState, useRef, useEffect } from 'react';
 
-function useTimeMachine<T>(state: T): [T, () => T | void] {
+function useTimeMachine<T>(state: T): [T | null, () => T | void] {
   const register = useRef<T[]>([]); //para que el array persista;
 
   const prevState = useRef<T | null>(null);
   const [currentState, setCurrentState] = useState<T>(state);
+
   useEffect(() => {
     register.current.unshift(state);
-    console.log(register);
     prevState.current = currentState;
     setCurrentState(state);
   }, [state]);
+  // const position = useRef(1);
+  // const [previousValue, setPreviousValue] = useState<T | null>(null);
 
-  //return prevState.current;
-  const position = useRef(0);
-  const previousValue = register.current[position.current];
+  //const previousValue = register.current[position.current];
 
+  // function getPreviousValue() {
+  //   if (
+  //     register.current.length > 0 &&
+  //     position.current < register.current.length
+  //   ) {
+  //     setPreviousValue(register.current[position.current]);
+
+  //     position.current += 1;
+  //   }
+  // } // a getPreviousVAlue podria pasarle un argumento para que me de 1 posicion mas antigua o
   function getPreviousValue() {
-    if (
-      register.current.length > 0 &&
-      position.current < register.current.length
-    ) {
-      console.log(register.current[position.current]);
-      position.current += 1;
-    }
+    console.log('domingo para todos ');
   }
 
-  return [previousValue, getPreviousValue];
+  // //una posición más reciente
+
+  return [
+    register.current.length > 1 ? register.current[1] : null,
+    getPreviousValue,
+  ];
 }
 
 export default useTimeMachine;
