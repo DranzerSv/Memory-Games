5f1a1d04cd6a94138b490f6d9d292c3d2be6ea58
Douglas Navarrete <dranzersv@gmail.com>
2023-03-23 13:58:42 -0600

    refactor: modified props of card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,15 +4,21 @@ import { ICardProps } from '../../interfaces/ICard';
 
 import '../Card/card.scss';
 
-export default function Card({ id, setSelectedCard }: ICardProps) {
+export default function Card({
+  id,
+  setSelectedCard,
+  isSelected,
+  setFocus,
+}: ICardProps) {
   return (
     <div
-      className={'square color' + id}
+      className={`square color${id} ${isSelected ? 'selected' : null}`}
       onClick={() => {
         setSelectedCard(id);
+        setFocus(id);
       }}
     >
-      <h1>{id}</h1>
+      <h1>{id + ' ' + isSelected}</h1>
     </div>
   );
 }
--- src/interfaces/ICard.tsx
+++ src/interfaces/ICard.tsx
@@ -3,4 +3,6 @@ import { Dispatch, SetStateAction } from 'react';
 export interface ICardProps {
   id: number;
   setSelectedCard: Dispatch<SetStateAction<number | null>>;
+  isSelected: boolean;
+  setFocus: Dispatch<SetStateAction<number | null>>;
 }
