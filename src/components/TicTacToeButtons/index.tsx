4b95dee5955d9d2937a464977139e233e1c2b1ee
Douglas Navarrete <dranzersv@gmail.com>
2023-03-26 16:20:56 -0600

    refactor: added current functions to buttons component

--- src/components/TicTacToeButtons/index.tsx
+++ src/components/TicTacToeButtons/index.tsx
@@ -5,10 +5,69 @@ interface ITicTacToeButtonsProps {
   historyPosition: number;
   setRenderedBoard: Dispatch<SetStateAction<(string | null)[] | null>>;
   setHistoryPosition: Dispatch<SetStateAction<number>>;
+  boardState: (string | null)[];
+  finished: boolean;
 }
 
 export default function TicTactToe({
   getPreviousValue,
+  historyPosition,
+  setRenderedBoard,
+  setHistoryPosition,
+  boardState,
+  finished,
 }: ITicTacToeButtonsProps) {
-  return <section className="machineButtons"></section>;
+  return (
+    <section className="machineButtons">
+      <button
+        className={getPreviousValue(historyPosition) === null ? 'disable' : ''}
+        onClick={() => {
+          const value = getPreviousValue(historyPosition);
+          if (value !== null) {
+            setRenderedBoard(value);
+            setHistoryPosition(historyPosition + 1);
+          }
+        }}
+      >
+        Previous
+      </button>
+
+      <button
+        className={historyPosition === 0 ? 'disable' : ''}
+        onClick={() => {
+          if (historyPosition !== 0) {
+            setRenderedBoard(getPreviousValue(historyPosition - 2));
+            setHistoryPosition(historyPosition - 1);
+          }
+        }}
+      >
+        Next
+      </button>
+      {finished ? (
+        <button
+          className={historyPosition === 0 ? 'disable' : ''}
+          onClick={() => {
+            if (historyPosition !== 0) {
+              setHistoryPosition(0);
+              setRenderedBoard(boardState);
+            }
+          }}
+        >
+          Replay
+        </button>
+      ) : (
+        <button
+          className={historyPosition === 0 ? 'disable' : ''}
+          onClick={() => {
+            if (historyPosition !== 0) {
+              setHistoryPosition(0);
+              setRenderedBoard(boardState);
+            }
+          }}
+        >
+          Resume
+        </button>
+      )}
+    </section>
+  );
 }
