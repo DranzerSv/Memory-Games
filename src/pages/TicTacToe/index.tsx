5253a7e187ce8ffc273620ca313e363409a4aaf6
Douglas Navarrete <dranzersv@gmail.com>
2023-03-26 11:22:45 -0600

    feature: implemente hook useTimeMachine

--- src/pages/TicTacToe/index.tsx
+++ src/pages/TicTacToe/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-import usePreviousState from '../../customHooks/usePreviousState';
+import useTimeMachine from '../../customHooks/useTimeMachine';
+
 import verifyWinner from './verifyWinner';
 
 import Square from '../../components/Square';
@@ -8,17 +9,18 @@ import Square from '../../components/Square';
 import './board.scss';
 
 function TicTactToe() {
-  const [boardState, setBoardState] = useState<Array<string | null>>([
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-  ]);
+  const emptyBoard = [null, null, null, null, null, null, null, null, null];
+
+  const [boardState, setBoardState] =
+    useState<Array<string | null>>(emptyBoard);
+  const [previousValue, getPreviousValue] = useTimeMachine(boardState);
+
+  const [renderedBoard, setRenderedBoard] = useState<Array<
+    string | null
+  > | null>(emptyBoard);
+
+  const [historyPosition, setHistoryPosition] = useState(0);
+
   verifyWinner(boardState);
 
   const [next, setNext] = useState<boolean>(true);
@@ -28,18 +30,55 @@ function TicTactToe() {
     newBoardState[index] = next ? 'X' : '0';
     setNext(!next);
     setBoardState(newBoardState);
+    setRenderedBoard(newBoardState);
   }
   return (
     <div>
+      <button
+        className={getPreviousValue(historyPosition) === null ? 'disable' : ''}
+        onClick={() => {
+          const value = getPreviousValue(historyPosition);
+          if (value !== null) {
+            setRenderedBoard(value);
+            setHistoryPosition(historyPosition + 1);
+          }
+        }}
+      >
+        Previous
+      </button>
+      <button
+        className={historyPosition === 0 ? 'disable' : ''}
+        onClick={() => {
+          if (historyPosition !== 0) {
+            setRenderedBoard(getPreviousValue(historyPosition - 2));
+            setHistoryPosition(historyPosition - 1);
+          }
+        }}
+      >
+        Next
+      </button>
+      <button
+        className={historyPosition === 0 ? 'disable' : ''}
+        onClick={() => {
+          if (historyPosition !== 0) {
+            setHistoryPosition(0);
+            setRenderedBoard(boardState);
+          }
+        }}
+      >
+        Resume
+      </button>
       <div className="board">
-        {boardState.map((boardPosition, index) => (
-          <Square
-            key={index}
-            id={index}
-            symbol={boardPosition}
-            handleClick={handleClick}
-          />
-        ))}
+        {renderedBoard !== null
+          ? renderedBoard.map((boardPosition, index) => (
+              <Square
+                key={index}
+                id={index}
+                symbol={boardPosition}
+                handleClick={handleClick}
+              />
+            ))
+          : null}
       </div>
     </div>
   );
