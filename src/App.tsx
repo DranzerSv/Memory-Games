3aa2be7c44857f24c565d0925c39260c503dde77
Douglas Navarrete <dranzersv@gmail.com>
2023-05-11 07:24:24 -0600

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ColorCards from './pages/ColorCards';
 import TicTactToe from './pages/TicTacToe';
 
 import './App.scss';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<ColorCards />} />
           <Route path="/phase2" element={<TicTactToe />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './footer.scss';
+
+export default function Footer() {
+  return <footer className="footer"> &copy; Douglas Navarrete 2023</footer>;
+}
